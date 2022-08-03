cab427269263de27216587058a4d4baa8aa30452
Lotric <fwas2016@mail.ru>
2022-08-03 12:15:14 +0300

    конец обучения 1

--- js/1script.js
+++ js/1script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+
+let  namberOfFilms = +prompt("сколко фильмов вы уже посмотрели?", "10");
+
+
+const personalMovieDB ={
+    count: namberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+let a = prompt("один из последних просмотренных фильмов?", ""),
+    b = +prompt("на сколько оценете его?", ""),
+    c = prompt("один из последних просмотренных фильмов?", ""),
+    d = +prompt("на сколько оценете его?", "");
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
